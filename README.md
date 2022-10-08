67e2cd42482bddd1725ec141d8042500584d9663
RuFrontEnd <k718198@gmail.com>
2022-10-08 16:45:09 +0800

    新增 css loader

--- README.md
+++
@@ -1,2 +0,0 @@
-# webpack-research
-webpack-research
--- official-guide/webpack.config.js
+++ official-guide/webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // 符合的副檔名
+        use: ["style-loader", "css-loader"], // 使用的 loader => 順序: style-loader -> css-loader (不照順序會出錯)
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
   },
 };
